ea7d844d560e00ff6718ada4613a885a50510e9c
JossinAntony <jossinde@gmail.com>
2020-10-11 09:55:28 +0200

    displayStudents.dart initialised.

---
+++ lib/displayStudents.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DisplayStudents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: null,
+      child: Icon(Icons.add)
+      ),
+    );
+  }
+}
---
+++ lib/providerdemo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:providerdemo_flutter_app/displayStudents.dart';
+
+class ProviderDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DisplayStudents(),
+    );
+  }
+}
